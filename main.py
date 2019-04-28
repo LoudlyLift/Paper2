1ba6308079a56bd5149d8c48d085d051d5edeb1c
Ivan Johnson <git@IvanJohnson.net>
2019-04-28 11:39:58 -0500

    Implement getStateMetadata

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
 import argparse
+import ast
 import numpy
+import sys
 import time
 
 import model
 import qtable
+import qlearning
+
+def Float2DMatrix(string):
+    mat = ast.literal_eval(string)
+    if type(mat) != list:
+        raise argparse.ArgumentTypeError(f'"{string!r}" is not a Float2DMatrix')
+
+    if(len(mat) == 0):
+        return mat
+
+    #width=len(mat[0])
+
+    for row in mat:
+        if type(row) != list:
+            raise argparse.ArgumentTypeError(f'"{string!r}" is not a Float2DMatrix')
+        #if len(row) != width:
+        #    raise argparse.ArgumentTypeError(f'"{string!r}" is not a Float2DMatrix, as some rows are different widths')
+        for entry in row:
+            if type(entry) != float:
+                raise argparse.ArgumentTypeError(f'"{string!r}" is not a Float2DMatrix')
+
+    return mat
 
 assert(__name__ == "__main__")
 
@@ -16,17 +40,30 @@ parser.add_argument('--log-period', type=int, default=100, help="How many steps 
 parser.add_argument('--num-steps', type=int, default=10000, help="How many steps to take")
 parser.add_argument('--fraction-randomized', type=float, default=0.1, help="What fraction ([0,1]) of actions should be chosen at random")
 parser.add_argument('--learning-rate', type=float, default=0.1, help="The Q-Table updates with a moving average. This is the weight of the most recent observation. Equivalent to 1-α from the paper.")
+parser.add_argument('--future-discount', type=float, default=0.5, help="What weight to place on future values; zero ignores all future consequences, values near one give them significant weight (Denoted by γ in the paper)")
 
 # MODEL
 parser.add_argument('--num-servers', type=int, default=3, help="The number of edge devices that are available for the IoT device to offload tasks to (M)")
 parser.add_argument('--num-parts', type=int, default=10, help="The number of parts that the task is split into (N_x)")
+parser.add_argument('--num-battery-levels', type=int, default=21, help="The number of levels to quantize the battery into")
+parser.add_argument('--num-harvest-levels', type=int, default=6,  help="The number of levels to quantize the estimate of harvested energy into")
+
+parser.add_argument('--max-battery', type=float, default=7.4, help="The maximum charge the battery can hold (Wh)")
+
+parser.add_argument('--transmission-rates', type=Float2DMatrix, default=[[3,4,5,6,7],[8,9,10,11,12],[5,6,7,9,10]], help="A list. For each server, this list contains another list that contains the possible link rates with that server. Must have ")
+
+#TODO: calculate this dynamically
+parser.add_argument('--max-harvest', type=float, default=1.7e-3, help="The maximum amount of energy that CAN be harvested in a single tick (only affects quantization levels? actual max depends on the other parameters)")
 
 args = parser.parse_args()
 
+if(len(args.transmission_rates) != args.num_servers):
+    print("The length of the list passed to --transmission-rates must equal the value of --num-servers", file=sys.stderr)
+    exit(1)
+
 class environment(model.model):
     def __init__(self, args):
-        super().__init__(args.num_servers, args.num_parts)
-        raise Exception("Not Implemented")
+        super().__init__(args.num_servers, args.num_parts, args.num_battery_levels, args.num_harvest_levels, args.transmission_rates)
 
     def step(self, actionNum):
         #super().step(selection, nOffload)
--- model.py
+++ model.py
@@ -6,29 +6,43 @@ class model:
 
     """
 
-    def __init__(self, nServers, nParts):
-        self.N_SERVERS = nServers
-        self.N_PARTS = nParts
+    def __init__(self, cServers, cParts, cBatteryLevels, cHarvestLevels,
+                 transmission_rates):
+        self.C_SERVERS = cServers
+        self.C_PARTS = cParts
+        self.C_BAT = cBatteryLevels
+        self.C_HARVEST = cHarvestLevels
+
+        self.TRANSMISSION_RATES = transmission_rates
+
+    def getState(self):
         raise Exception("Not Implemented")
 
     def reset(self):
+        #TODO: randomize variables
         raise Exception("Not Implemented")
+        return self.getState()
 
     def getStateMetadata(self):
-        raise Exception("Not Implemented")
+        return tuple(len(foo) for foo in self.TRANSMISSION_RATES) + (self.C_HARVEST, self.C_BAT)
 
     def step(self, selection, nOffload):
         """Simulates a single timestep. The device elects to offload `nOffload`
-        of the `N_PARTS` parts to the server indicated by the index `selection`.
+        of the `C_PARTS` parts to the server indicated by the index `selection`.
 
         """
         assert(type(nOffload) == int)
-        assert(0 <= nOffload and nOffload <= self.N_PARTS) #equality on both
+        assert(0 <= nOffload and nOffload <= self.C_PARTS) #equality on both
 
         assert(type(selection) == int)
-        assert(0 <= selection and selection < self.N_SERVERS)
+        assert(0 <= selection and selection < self.C_SERVERS)
 
         raise Exception("Not Implemented")
+
+        state = getState()
+
+        done = False
+        return (state, reward, done)
 
     def closeEpisode(self):
         """Returns a list. That contains, for each time step, the tuple (energy
