9e1084631c3d0c7f958b991fcec1d4873360c6d7
Ivan Johnson <git@IvanJohnson.net>
2019-04-28 16:25:23 -0500

    Partially implement model's reset function

--- model.py
+++ model.py
@@ -1,10 +1,28 @@
 import numpy
 
+import markov
+
 class model:
     """This class manages the state of the system in response to the device's
     actions, and evaluates the utility function.
 
     """
+
+    class connection(markov.chain):
+        def __init__(self, rates):
+            self.rates = rates
+            self.cStates = len(self.rates)
+            mat = [ [ 1 / self.cStates for dest in range(self.cStates) ]
+                    for src in range(self.cStates)]
+
+            super().__init__(mat)
+
+        def step(self):
+            index = super().step()
+            return self.rates(cStates)
+
+        def statecount(self):
+            return self.cStates
 
     def __init__(self, cServers, cParts, cBatteryLevels, cHarvestLevels,
                  transmission_rates):
@@ -13,18 +31,16 @@ class model:
         self.C_BAT = cBatteryLevels
         self.C_HARVEST = cHarvestLevels
 
-        self.TRANSMISSION_RATES = transmission_rates
-
     def getState(self):
         raise Exception("Not Implemented")
 
     def reset(self):
-        #TODO: randomize variables
-        raise Exception("Not Implemented")
+        #TODO?
+        self.connections = [ model.connection(rates) for rates in transmission_rates ]
         return self.getState()
 
     def getStateMetadata(self):
-        return tuple(len(foo) for foo in self.TRANSMISSION_RATES) + (self.C_HARVEST, self.C_BAT)
+        return tuple(con.statecount() for con in self.connections) + (self.C_HARVEST, self.C_BAT)
 
     def step(self, selection, nOffload):
         """Simulates a single timestep. The device elects to offload `nOffload`
